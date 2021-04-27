f862579b6536417842d424b383e97a369222cd27
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-04-27 08:42:04 +0500

    Add book style

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useQuery, gql } from '@apollo/client'
+import "./App.css"
+import Books from "./componints/books"
 
-const QUERY = gql`
-  {
-    books {
-      name
-      price
-      id
-      author {
-        name
-      }
-    }
-  }
-`
 
 function App () {
 
-  const { loading, error, data } = useQuery(QUERY)
-  console.log(data);
-
-  if (loading) return <>loading..</>
-  if (error) return <>{error.message}</>
 
   return (
     <>
-      <ul>
-        {
-          data.books.map(book => (
-            <li key={book.id}>{book.name}</li>
-          ))
-        }
-      </ul>
+      <Books />
     </>
   )
 }
---
+++ src/componints/books.jsx
@@ -0,0 +1,49 @@
+import { useQuery, gql } from '@apollo/client'
+
+import "./books.css"
+
+const QUERY = gql`
+  {
+    books {
+      name
+      price
+      id
+      author {
+        name
+      }
+    }
+  }
+`
+
+function Books () {
+
+  const { loading, error, data } = useQuery(QUERY)
+
+  if (loading) return <>loading..</>
+  if (error) return <>{error.message}</>
+
+  return (
+    <div className='container'>
+
+      <ul className="books">
+        {
+          data.books.map(book => (
+            <li key={book.id} className="book">
+              <img src="https://via.placeholder.com/250x150" alt={book.name} className="book__img"/>
+              <div>
+                <p className="book__name"><b>Book Name: </b>{book.name}</p>
+                <p className="book_author"><b>Book Author: </b>{book.author.name}</p>
+              </div>
+              <div className="book_price-wrapper">
+                <span><b>Book Price: </b>{book.price}</span>
+                <button type="submit" className="book__but-btn">BUY</button>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Books
